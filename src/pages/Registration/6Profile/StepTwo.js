62036431d3cc623bb1ee105fa4ef244dc57a292a
michaelzomoakin <44997832+makindiilete@users.noreply.github.com>
2022-01-23 19:51:57 +0100

    Update StepTwo.js

--- src/pages/Registration/6Profile/StepTwo.js
+++ src/pages/Registration/6Profile/StepTwo.js
@@ -163,7 +163,7 @@ const StepTwo = ({ currentStep, setCurrentStep, title, subTitle }) => {
                   alt=""
                   style={{
                     width: "100%",
-                    height: "100%",
+                    height: "15.8rem",
                     objectFit: "cover",
                   }}
                 />
