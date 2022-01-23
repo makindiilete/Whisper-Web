0fdef5b305dea1218aa18c3e9e91a75cf271f0e8
michaelzomoakin <44997832+makindiilete@users.noreply.github.com>
2022-01-24 00:14:25 +0100

    updates

--- src/pages/Registration/12CustomerPreferences.page.js
+++ src/pages/Registration/12CustomerPreferences.page.js
@@ -7,6 +7,7 @@ import { FaAngleLeft } from "react-icons/all";
 import styles from "../../assets/css/auth/yourAttributes.module.css";
 import { imInto, providingTo } from "../../components/dataSets";
 import routes from "../../routes";
+import { Input, Slider } from "antd";
 
 const CustomerPreferencesPage = (props) => {
   let location = useLocation();
@@ -17,6 +18,13 @@ const CustomerPreferencesPage = (props) => {
   }, [location.pathname]);
   const [isLoading, setIsLoading] = useState(false);
   const [selected, setSelected] = useState([]);
+  const [currentStep, setCurrentStep] = useState(1);
+  const [age, setAge] = useState({
+    startRange: 0,
+    endRange: 100,
+  });
+  const [km, setKm] = useState(5);
+  const [amount, setAmount] = useState();
 
   const addRemoveItem = (item) => {
     //Add
@@ -30,6 +38,131 @@ const CustomerPreferencesPage = (props) => {
       setSelected(filterItem);
     }
   };
+
+  const slider = {
+    width: "100%",
+    height: "4.8rem",
+    border: "1px solid #DADADA",
+    borderRadius: "8px",
+    padding: "1.7rem 1rem",
+    marginTop: mobile ? "-2rem" : null,
+    // display: "flex",
+    // justifyContent: "center",
+    // alignItems: "center",
+  };
+  /*
+  const sliderPc = {
+    width: "100%",
+    height: "6.4rem",
+    border: "1px solid #DADADA",
+    borderRadius: "8px"
+  }*/
+
+  function onChange(value) {
+    setAge({ startRange: value[0], endRange: value[1] });
+  }
+
+  function onAfterChange(value) {
+    setAge({ startRange: value[0], endRange: value[1] });
+  }
+
+  function onChangeKm(value) {
+    setKm(value);
+  }
+
+  function onAfterChangeKm(value) {
+    setKm(value);
+  }
+
+  const firstStep = () => {
+    return (
+      <div className={`row `}>
+        <div className={`col-md-8 offset-md-2 ${styles.attributesCol}`}>
+          <h5>I am into</h5>
+          <div className={styles.flexrowbetween}>
+            {imInto?.map((item) => (
+              <button
+                key={item}
+                className={`${styles.attrBtn} ${
+                  selected?.includes(item) ? styles.attrBtnActive : null
+                }`}
+                onClick={() => addRemoveItem(item)}
+              >
+                {item}
+              </button>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const secondStep = () => {
+    return (
+      <>
+        <div className={`row `}>
+          <div className={`col-md-6 mb-2 mb-md-0 ${styles.attributesCol}`}>
+            <div className={styles.flexrowbetween}>
+              <h5>Age</h5>
+              <h5>
+                {" "}
+                {age.startRange} - {age.endRange}{" "}
+              </h5>
+            </div>
+            <div style={slider}>
+              <Slider
+                range
+                step={1}
+                defaultValue={[18, 25]}
+                onChange={onChange}
+                onAfterChange={onAfterChange}
+                min={18}
+              />
+            </div>
+          </div>
+
+          <div className={`col-md-6 mb-2 mb-md-0 ${styles.attributesCol}`}>
+            <div className={styles.flexrowbetween}>
+              <h5>Distance</h5>
+              <h5> {km} Km </h5>
+            </div>
+            <div style={slider}>
+              <Slider
+                step={1}
+                defaultValue={5}
+                onChange={onChangeKm}
+                onAfterChange={onAfterChangeKm}
+                min={1}
+              />
+            </div>
+          </div>
+        </div>
+        <div className={`row `}>
+          <div className={`col-md-6  mb-2 mb-md-0 ${styles.attributesCol}`}>
+            <div>
+              <h5>
+                What is the least amount you are looking to spend per hour?
+              </h5>
+            </div>
+            <div className="d-flex align-items-center">
+              <Input onChange={(e) => setAmount(`$ ${e.target.value}`)} />
+              <h5 className="appendInput">/hr</h5>
+            </div>
+            {/* <Input.Search
+              // addonBefore="https://"
+              addonAfter="/hr"
+              // onSearch={onSearch}
+            />*/}
+          </div>
+
+          <div
+            className={`col-md-6 mb-2 mb-md-0 ${styles.attributesCol}`}
+          ></div>
+        </div>
+      </>
+    );
+  };
+
   return (
     <AuthContainerPage>
       {isLoading ? (
@@ -59,24 +192,7 @@ const CustomerPreferencesPage = (props) => {
                 <h4 className="text-center mt-5 mt-md-0">Your Preferences</h4>
                 <p className="text-center">What are your preferences?</p>
               </div>
-              <div className={`row `}>
-                <div className={`col-md-8 offset-md-2 ${styles.attributesCol}`}>
-                  <h5>I am into</h5>
-                  <div className={styles.flexrowbetween}>
-                    {imInto?.map((item) => (
-                      <button
-                        key={item}
-                        className={`${styles.attrBtn} ${
-                          selected?.includes(item) ? styles.attrBtnActive : null
-                        }`}
-                        onClick={() => addRemoveItem(item)}
-                      >
-                        {item}
-                      </button>
-                    ))}
-                  </div>
-                </div>
-              </div>
+              {currentStep === 1 ? firstStep() : secondStep()}
               <div className="row">
                 <div
                   className={`col-md-6 offset-md-3 ${styles.attributesCol} `}
@@ -84,11 +200,15 @@ const CustomerPreferencesPage = (props) => {
                   <button
                     className={`btn btn-primary btn-block`}
                     disabled={selected.length === 0}
-                    /*onClick={() =>
-                      history.push(routes.selectTypeOfServiceToProvide)
-                    }*/
+                    onClick={() => {
+                      if (currentStep === 1) {
+                        setCurrentStep(2);
+                      } else {
+                        // history.push(routes.customerPreferences)
+                      }
+                    }}
                   >
-                    Continue
+                    {currentStep === 1 ? "Continue" : "Find yourself Someone"}
                   </button>
                 </div>
               </div>
--- src/pages/Registration/12ProviderPreferences.page.js
+++ src/pages/Registration/12ProviderPreferences.page.js
@@ -8,6 +8,7 @@ import { FaAngleLeft } from "react-icons/all";
 import styles from "../../assets/css/auth/yourAttributes.module.css";
 import { imInto } from "../../components/dataSets";
 import routes from "../../routes";
+import { Input, Slider } from "antd";
 
 const ProviderPreferencesPage = (props) => {
   let location = useLocation();
@@ -17,6 +18,161 @@ const ProviderPreferencesPage = (props) => {
     window.scrollTo(0, 0);
   }, [location.pathname]);
   const [isLoading, setIsLoading] = useState(false);
+  const [selected, setSelected] = useState([]);
+  const [currentStep, setCurrentStep] = useState(1);
+  const [age, setAge] = useState({
+    startRange: 0,
+    endRange: 100,
+  });
+  const [km, setKm] = useState(5);
+  const [amount, setAmount] = useState();
+
+  const addRemoveItem = (item) => {
+    //Add
+    if (!selected?.includes(item)) {
+      setSelected([...selected, item]);
+    }
+
+    //Remove
+    else {
+      const filterItem = selected?.filter((i) => i !== item);
+      setSelected(filterItem);
+    }
+  };
+
+  const slider = {
+    width: "100%",
+    height: "4.8rem",
+    border: "1px solid #DADADA",
+    borderRadius: "8px",
+    padding: "1.7rem 1rem",
+    marginTop: mobile ? "-2rem" : null,
+    // display: "flex",
+    // justifyContent: "center",
+    // alignItems: "center",
+  };
+  function onChange(value) {
+    setAge({ startRange: value[0], endRange: value[1] });
+  }
+
+  function onAfterChange(value) {
+    setAge({ startRange: value[0], endRange: value[1] });
+  }
+
+  function onChangeKm(value) {
+    setKm(value);
+  }
+
+  function onAfterChangeKm(value) {
+    setKm(value);
+  }
+
+  const firstStep = () => {
+    return (
+      <div className={`row `}>
+        <div className={`col-md-8 offset-md-2 ${styles.attributesCol}`}>
+          <h5>I am into</h5>
+          <div className={styles.flexrowbetween}>
+            {imInto?.map((item) => (
+              <button
+                key={item}
+                className={`${styles.attrBtn} ${
+                  selected?.includes(item) ? styles.attrBtnActive : null
+                }`}
+                onClick={() => addRemoveItem(item)}
+              >
+                {item}
+              </button>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const secondStep = () => {
+    return (
+      <>
+        <div className={`row `}>
+          <div className={`col-md-6 mb-2 mb-md-0 ${styles.attributesCol}`}>
+            <div className={styles.flexrowbetween}>
+              <h5>Age</h5>
+              <h5>
+                {" "}
+                {age.startRange} - {age.endRange}{" "}
+              </h5>
+            </div>
+            <div style={slider}>
+              <Slider
+                range
+                step={1}
+                defaultValue={[18, 25]}
+                onChange={onChange}
+                onAfterChange={onAfterChange}
+                min={18}
+              />
+            </div>
+          </div>
+
+          <div className={`col-md-6 mb-2 mb-md-0 ${styles.attributesCol}`}>
+            <div className={styles.flexrowbetween}>
+              <h5>Distance</h5>
+              <h5> {km} Km </h5>
+            </div>
+            <div style={slider}>
+              <Slider
+                step={1}
+                defaultValue={5}
+                onChange={onChangeKm}
+                onAfterChange={onAfterChangeKm}
+                min={1}
+              />
+            </div>
+          </div>
+        </div>
+        <div className={`row `}>
+          <div className={`col-md-6 mb-2 mb-md-0 ${styles.attributesCol}`}>
+            <div>
+              <h5>Set a price/rate for your services</h5>
+            </div>
+            <div className="d-flex align-items-center">
+              <div className="prependInput">
+                <small>Companion</small>
+              </div>
+              <Input onChange={(e) => setAmount(`$ ${e.target.value}`)} />
+              <h5 className="appendInput">/hr</h5>
+            </div>
+            <div className="flexrowbetweencenter">
+              <div
+                style={{
+                  width: "1rem",
+                  height: "1rem",
+                  borderRadius: "0.3rem",
+                  backgroundColor: "#7917CE",
+                }}
+              />
+              <p className="padding-none">
+                10% of your hourly rate will be charged as commission
+              </p>
+            </div>
+          </div>
+
+          <div className={`col-md-6 mb-2 mb-md-0 ${styles.attributesCol}`}>
+            <div>
+              <h5 className="text-white">Set a price/rate for your services</h5>
+            </div>
+            <div className="d-flex align-items-center">
+              <div className="prependInput">
+                <small>X-Rated</small>
+              </div>
+              <Input onChange={(e) => setAmount(`$ ${e.target.value}`)} />
+              <h5 className="appendInput">/hr</h5>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
   return (
     <AuthContainerPage>
       {isLoading ? (
@@ -46,19 +202,23 @@ const ProviderPreferencesPage = (props) => {
                 <h4 className="text-center mt-5 mt-md-0">Your Preferences</h4>
                 <p className="text-center">What are your preferences?</p>
               </div>
-              <h2 className="text-center">Content</h2>
+              {currentStep === 1 ? firstStep() : secondStep()}
               <div className="row">
                 <div
                   className={`col-md-6 offset-md-3 ${styles.attributesCol} `}
                 >
                   <button
                     className={`btn btn-primary btn-block`}
-                    // disabled={!selected}
-                    /*  onClick={() =>
-                                            history.push(routes.selectTypeOfServiceToProvide)
-                                        }*/
+                    disabled={selected.length === 0}
+                    onClick={() => {
+                      if (currentStep === 1) {
+                        setCurrentStep(2);
+                      } else {
+                        // history.push(routes.customerPreferences)
+                      }
+                    }}
                   >
-                    Find yourself Someone
+                    {currentStep === 1 ? "Continue" : "Find yourself Someone"}
                   </button>
                 </div>
               </div>
