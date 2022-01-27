eafd4735296f7ae748bdc0f3f66515c169c84c04
michaelzomoakin <44997832+makindiilete@users.noreply.github.com>
2022-01-27 01:19:10 +0100

    provider home done

--- src/pages/Home/ProviderHome.page.js
+++ src/pages/Home/ProviderHome.page.js
@@ -1,13 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useHistory } from "react-router-dom";
 import useMobile from "../../hooks/useMobile";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import * as icons from "@fortawesome/free-solid-svg-icons";
 import { HomeContainerPage } from "./HomeContainer.page";
 import styles from "../../assets/css/providerHome.module.css";
 import img1 from "../../assets/images/homeInApp/Rectangle 2685.svg";
 import img2 from "../../assets/images/homeInApp/Rectangle 13 copy.svg";
-import img3 from "../../assets/images/homeInApp/Rectangle 13.png";
+import img3 from "../../assets/images/homeInApp/Rectangle 2685 copy 3.svg";
 import img4 from "../../assets/images/homeInApp/Rectangle 13.svg";
 import img5 from "../../assets/images/homeInApp/Rectangle 2685 copy 4.svg";
+import img6 from "../../assets/images/homeInApp/Rectangle 2685 copy 2.svg";
+import reverse from "../../assets/images/homeInApp/Group 754.svg";
+import like from "../../assets/images/homeInApp/love.svg";
+import decline from "../../assets/images/homeInApp/decline.svg";
 import { Badge } from "../../components/Badge";
 
 const ProviderHomePage = (props) => {
@@ -17,6 +23,22 @@ const ProviderHomePage = (props) => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [location.pathname]);
+  const [index, setIndex] = useState(1);
+  const [images, setImages] = useState([img1, img3, img6]);
+  const [activeImage, setActiveImage] = useState(images[0]);
+
+  const handleMoveBack = () => {
+    const indx = index === 1 ? images.length : index - 1;
+    setIndex(indx);
+    setActiveImage(images[indx - 1]);
+  };
+
+  const handleMoveNext = () => {
+    const indx = index === images.length ? 1 : index + 1;
+    setIndex(indx);
+    setActiveImage(images[indx - 1]);
+  };
+
   return (
     <HomeContainerPage>
       <div className="row">
@@ -26,10 +48,44 @@ const ProviderHomePage = (props) => {
           <div className={styles.profileDetailsContainer}>
             <div className="row">
               <div className="col-md-6 p-0 position-relative">
+                <div className="currentTotalImg">
+                  <div className="w-100 h-100 d-flex justify-content-center align-items-center">
+                    <h5 className="text-white padding-none">
+                      {`${index}/${images.length}`}
+                    </h5>
+                  </div>
+                </div>
+                <div className="prevNextImg">
+                  <div className="w-100 d-flex justify-content-between px-3">
+                    <div className="arrow backArrow" onClick={handleMoveBack}>
+                      <FontAwesomeIcon
+                        icon={icons.faChevronLeft}
+                        size="1x"
+                        className="text-white"
+                      />
+                    </div>
+                    <div className="arrow nextArrow" onClick={handleMoveNext}>
+                      <FontAwesomeIcon
+                        icon={icons.faChevronRight}
+                        size="1x"
+                        className="text-white"
+                      />
+                    </div>
+                  </div>
+                </div>
+                {/* /.prevNext */}
                 <div
                   className={`${styles.galleryImg} ${styles.galleryImgOverlay}`}
                 />
-                <img src={img1} alt="" className={styles.galleryImg} />
+                <img src={activeImage} alt="" className={styles.galleryImg} />
+                <div className="actions">
+                  <div className="actions__container">
+                    <img src={reverse} className="img-fluid" alt="" />
+                    <img src={like} className="img-fluid" alt="" />
+                    <img src={decline} className="img-fluid" alt="" />
+                  </div>
+                </div>
+                {/* /.actions */}
               </div>
               {/* /.col-md-6 */}
               <div className={`col-md-6 ${styles.profileContainerRightCol}`}>
@@ -66,30 +122,62 @@ const ProviderHomePage = (props) => {
         <div className={`col-md-4 ${styles.leftColumn}`}>
           <h4>Latest Like/Request</h4>
           <br />
-          <div className="d-flex flex-wrap">
-            <div>
+          <div
+            className="d-flex flex-wrap"
+            style={mobile ? { marginRight: "-2rem" } : null}
+          >
+            <div className="position-relative">
               <div
                 className={`${styles.galleryImgThumbnail} ${styles.galleryImgThumbnailOverlay}`}
               />
               <img src={img2} alt="" className={styles.galleryImgThumbnail} />
+              <div className={styles.galleryImgThumbnailDesc}>
+                <small className="font-weight-bold text-white">
+                  Jack Smith 27
+                </small>
+                <br />
+                <small className="text-white">Business Man</small>
+              </div>
+              {/* /.font-weight-bold */}
             </div>
-            <div>
+            <div className="position-relative">
               <div
                 className={`${styles.galleryImgThumbnail} ${styles.galleryImgThumbnailOverlay}`}
               />
               <img src={img3} alt="" className={styles.galleryImgThumbnail} />
+              <div className={styles.galleryImgThumbnailDesc}>
+                <small className="font-weight-bold text-white">
+                  Manny Rice 29
+                </small>
+                <br />
+                <small className="text-white">Software Engineer</small>
+              </div>
             </div>
-            <div>
+            <div className="position-relative">
               <div
                 className={`${styles.galleryImgThumbnail} ${styles.galleryImgThumbnailOverlay}`}
               />
               <img src={img4} alt="" className={styles.galleryImgThumbnail} />
+              <div className={styles.galleryImgThumbnailDesc}>
+                <small className="font-weight-bold text-white">
+                  Ben Saheed 31
+                </small>
+                <br />
+                <small className="text-white">Rock Star</small>
+              </div>
             </div>
-            <div>
+            <div className="position-relative">
               <div
                 className={`${styles.galleryImgThumbnail} ${styles.galleryImgThumbnailOverlay}`}
               />
               <img src={img5} alt="" className={styles.galleryImgThumbnail} />
+              <div className={styles.galleryImgThumbnailDesc}>
+                <small className="font-weight-bold text-white">
+                  Azeez Fahn 29
+                </small>
+                <br />
+                <small className="text-white">Stock Broker</small>
+              </div>
             </div>
           </div>
         </div>
