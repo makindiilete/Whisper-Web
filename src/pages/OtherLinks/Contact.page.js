5fdb53d79efba65391051f2ab68cb93d46920f0c
michaelzomoakin <44997832+makindiilete@users.noreply.github.com>
2022-02-07 13:54:27 +0100

    Update Contact.page.js

--- src/pages/OtherLinks/Contact.page.js
+++ src/pages/OtherLinks/Contact.page.js
@@ -1,8 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useHistory } from "react-router-dom";
 import useMobile from "../../hooks/useMobile";
 import OtherLinksContainer from "./OtherLinksContainer";
 import background from "../../assets/images/others/getInTouch.jpg";
+import { Form, Input } from "antd";
+import routes from "../../routes";
+import SuccessModal from "../../components/Modals/successModal";
+import warningImg from "../../assets/images/auth/warning.svg";
 
 const ContactPage = (props) => {
   let location = useLocation();
@@ -11,6 +15,22 @@ const ContactPage = (props) => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [location.pathname]);
+  const [form] = Form.useForm();
+  const [showSuccess, setShowSuccess] = useState(false);
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (name, value) => {
+    setData({ ...data, [name]: value });
+  };
+
+  const handleSubmit = (values) => {
+    setShowSuccess(true);
+    form.resetFields();
+  };
   return (
     <>
       <OtherLinksContainer>
@@ -49,6 +69,93 @@ const ContactPage = (props) => {
             </div>
           </div>
         </div>
+        <br />
+        <br />
+        <br />
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 mb-2 mb-md-0">
+              <h5>Address</h5>
+              <small>4517 Washington Ave. Manchester, Kentucky 39495</small>
+              <br />
+              <br />
+              <h5>Phone Number</h5>
+              <small>(480) 555-0103</small>
+              <br />
+              <small>(704) 555-0127</small>
+            </div>
+            <div className="col-md-6">
+              <Form
+                layout="vertical"
+                scrollToFirstError
+                onFinish={handleSubmit}
+                form={form}
+              >
+                <Form.Item
+                  className="mb-3 mb-md-0 mt-2"
+                  initialValue=""
+                  name="name"
+                  label="Name"
+                  required
+                  rules={[{ required: true, message: "Required field" }]}
+                >
+                  <Input
+                    onChange={(e) => handleChange("name", e.target.value)}
+                  />
+                </Form.Item>
+                <Form.Item
+                  className="mb-3 mb-md-0 mt-2"
+                  initialValue=""
+                  name="email"
+                  label="Email"
+                  required
+                  rules={[
+                    {
+                      required: true,
+                      message: "Required field",
+                    },
+                    {
+                      type: "email",
+                      message: "Enter a valid email address",
+                    },
+                  ]}
+                >
+                  <Input
+                    onChange={(e) => handleChange("email", e.target.value)}
+                  />
+                </Form.Item>
+                <Form.Item
+                  className="mb-3 mb-md-0 mt-2"
+                  initialValue=""
+                  name="message"
+                  label="Message"
+                  required
+                  rules={[{ required: true, message: "Required field" }]}
+                >
+                  <Input.TextArea
+                    rows={5}
+                    onChange={(e) => handleChange("message", e.target.value)}
+                  />
+                </Form.Item>
+                <br />
+                <div className="d-md-flex justify-content-md-end">
+                  <button
+                    className={`btn btn-primary ${mobile && "btn-block"}`}
+                  >
+                    Submit
+                  </button>
+                </div>
+                <br />
+              </Form>
+            </div>
+          </div>
+        </div>
+        <SuccessModal
+          title="Yay!"
+          subtitle="Form submitted successfully"
+          visible={showSuccess}
+          onCancel={() => setShowSuccess(false)}
+        />
       </OtherLinksContainer>
     </>
   );
